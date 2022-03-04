ef8cca3f628776ba65db9b8ad957467234a15b01
22037 <srdesign22037@gmail.com>
2022-03-04 09:45:40 -0700

    Create CDE2Team22037.py
    Adding CDE2 (binning example with open CV) to the examples folder for all of us to use

---
+++ examples/CDE2Team22037.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+def rebin(arr, new_shape):
+    shape = (new_shape[0], arr.shape[0] // new_shape[0],
+             new_shape[1], arr.shape[1] // new_shape[1])
+    return arr.reshape(shape).mean(-1).mean(1)
+
+
+while True: 
+    
+    ret,img=cap.read()
+    # dimensions = img.shape
+    # print(dimensions)
+
+    imgR = img[:,:,0]
+    imgG = img[:,:,1]
+    imgB = img[:,:,2]
+
+    imgR = np.cast['uint32'](imgR)
+    imgR = rebin(imgR, (20,20))
+    imgG = np.cast['uint32'](imgG)
+    imgG = rebin(imgG, (20,20))
+    imgB = np.cast['uint32'](imgB)
+    imgB = rebin(imgB, (20,20))
+    imgQuant = imgG/imgB
+
+    #imgR = cv2.resize(imgR, [200,200])
+    cv2.imshow('Red Channel', imgR)
+    #imgG = cv2.resize(imgG, [200,200])
+    cv2.imshow('Green Channel', imgG)
+    #imgB = cv2.resize(imgB, [200,200])
+    cv2.imshow('Blue Channel', imgB)
+    #imgQuant = cv2.resize(imgQuant, [200,200])
+    cv2.imshow('Division Channel', imgB)
+    
+    if(cv2.waitKey(10) & 0xFF == ord('q')):
+        break
+
+
+cv2.imwrite("Red Channel.tiff",imgR)
+cv2.imwrite("Green Channel.tiff",imgG)
+cv2.imwrite("Blue Channel.tiff",imgB)
+cv2.imwrite("Division Channel.tiff",imgQuant)
