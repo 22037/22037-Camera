3770d524195628df5d541c5ccfd63d0ed16b0bf4
22037 <srdesign22037@gmail.com>
2022-03-22 18:02:07 -0700

    Update Blood_Quantification.py

--- examples/Blood_Quantification.py
+++ examples/Blood_Quantification.py
@@ -4,6 +4,15 @@ import cv2
 import numpy as np
 import time
 
+# Variables
+wavelength1 = 540
+wavelength2 = 680
+I_white1 = np.random(720,540)
+I_white2 = np.random(720,540)
+b1_w1 = 50
+b2_w2 = 25
+g1 = b1_w1/b2_w2
+
 cap = cv2.VideoCapture(0)
 
 while True: 
@@ -14,5 +23,7 @@ while True:
     # print(dimensions)
 
     
+    
+
     if(cv2.waitKey(10) & 0xFF == ord('q')):
         break
