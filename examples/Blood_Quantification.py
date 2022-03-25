5085603a7323a86a2b7088f260d4d807b4ad6a25
22037 <srdesign22037@gmail.com>
2022-03-25 15:02:51 -0700

    Code Integration - CL
    New Sample codes of blood quantification integration

--- examples/Blood_Quantification.py
+++ examples/Blood_Quantification.py
@@ -6,15 +6,15 @@ import time
 
 # Variables
 # Optical constants are given in the units of cm^-1 (dependent on the wavelength)
-h_blood_const =
+# h_blood_const =
 w_const = 2.79*10**(-3)
-m_const = 
-f_const = 
-mu_a =
-mu_s =
+# m_const = 
+# f_const = 
+# mu_a =
+# mu_s =
 wavelength1 = 540
 wavelength2 = 680
-h_blood = 
+# h_blood = 
 I_white1 = np.random(720,540)
 I_white2 = np.random(720,540)
 b1_w1 = 50
@@ -31,11 +31,12 @@ while True:
     current_time = time.time()
 
     ret,img = cap.read()
+
+    imgR = img[:,:,0]
+    imgG = img[:,:,1]
+
     # dimensions = img.shape
     # print(dimensions)
-
-    
-    
 
     if(cv2.waitKey(10) & 0xFF == ord('q')):
         break
