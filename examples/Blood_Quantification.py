982f942a13774ae7337f63e9f41f13c46fd815e8
hilltailor <srdesign22037@gmail.com>
2022-03-25 14:25:53 -0700

    Update Blood_Quantification.py

--- examples/Blood_Quantification.py
+++ examples/Blood_Quantification.py
@@ -5,6 +5,16 @@ import numpy as np
 import time
 
 # Variables
+# Optical constants are given in the units of cm^-1 (dependent on the wavelength)
+h_blood_const =
+w_const = 2.79*10**(-3)
+m_const = 
+f_const = 
+mu_a =
+mu_s =
+wavelength1 = 540
+wavelength2 = 680
+h_blood = 
 wavelength1 = 540
 wavelength2 = 680
 I_white1 = np.random(720,540)
