6782f6f61784a821fa187cca6413ffd78ab8fc94
Devesh Khosla <dekhosla86@gmail.com>
2022-04-15 15:14:01 -0700

    phase12

--- qt.py
+++ qt.py
@@ -286,64 +286,6 @@ class qt(QMainWindow):
         width = res[0]
         measure_time = 5.0 # average measurements over 5 secs
         camera_index = 0 # default camera starts at 0 by operating system
-
-        #read in curvefit files
-        #background=np.loadtxt('background', dtype='uint8', delimiter=',')
-
-
-      
-
-        # #images are stored in BSstandard folder
-        # fit0=np.loadtxt('fit0_2', dtype='float32', delimiter=',')
-        # fit1=np.loadtxt('fit1_2', dtype='float32', delimiter=',')
-        # fit2=np.loadtxt('fit2_2', dtype='float32', delimiter=',')
-        # fit3=np.loadtxt('fit3_2', dtype='float32', delimiter=',')
-        # fit4=np.loadtxt('fit4_2', dtype='float32', delimiter=',')
-        # fit5=np.loadtxt('fit5_2', dtype='float32', delimiter=',')
-        # fit6=np.loadtxt('fit6_2', dtype='float32', delimiter=',')
-        # fit7=np.loadtxt('fit7_2', dtype='float32', delimiter=',')
-        # fit8=np.loadtxt('fit8_2', dtype='float32', delimiter=',')
-        # fit9=np.loadtxt('fit9_2', dtype='float32', delimiter=',')
-        # fit10=np.loadtxt('fit10_2', dtype='float32', delimiter=',')
-        # #comment out 11 and 13
-        # fit11=np.loadtxt('fit12_2', dtype='float32', delimiter=',')
-        # fit12=np.loadtxt('fit12_2', dtype='float32', delimiter=',')
-        # fit13=np.loadtxt('background', dtype='float32', delimiter=',')
-
-        # #Processing
-        # looptime = 0.0
-        # use_queue = True
-        # data_cube = np.zeros((14, 540, 720), dtype=np.uint8)
-        # flatfield = np.zeros((14, 540, 720), dtype=np.float32)
-        # flatfield[0,:,:] = fit0
-        # flatfield[1,:,:] = fit1
-        # flatfield[2,:,:] = fit2
-        # flatfield[3,:,:] = fit3
-        # flatfield[4,:,:] = fit4
-        # flatfield[5,:,:] = fit5
-        # flatfield[6,:,:] = fit6
-        # flatfield[7,:,:] = fit7
-        # flatfield[8,:,:] = fit8
-        # flatfield[9,:,:] = fit9
-        # flatfield[10,:,:] = fit10
-        # flatfield[11,:,:] = fit11
-        # flatfield[12,:,:] = fit12
-        # flatfield[13,:,:] = fit13
-        # background = np.zeros((540, 720), dtype=np.uint8)
-        # #background = plt.imread("C13-BKGND.tiff")
-        # #flatfield = np.cast['uint8'](2**8.*np.random.random((540,720)))
-        # data_cube_corr = np.zeros((14, 540, 720), 'uint16')
-        # frame = np.zeros((540,720), dtype=np.uint8)
-        # self.data_cube_corr = np.zeros((14, 540, 720), 'uint16')
-        # self.frame = np.zeros((540,720), dtype=np.uint8)
-
-        #NEW
-        # bg_delta: tuple = (64, 64)
-        # bg_dx = bg_delta[1]
-        # bg_dy = bg_delta[0]
-
-        #Camera configuration file
-        #from configs.blackfly_configs  import configs
         
         # Display
         window_name    = 'Camera'
@@ -353,7 +295,7 @@ class qt(QMainWindow):
         fontScale      = 1
         fontColor      = (0,0,255)
         lineType       = 2
-        # cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE) # or WINDOW_NORMAL
+
         main_window_name         = 'Captured'
         binned_window_name       = 'Binned'
         processed_window_name    = 'Band-Passed'
@@ -365,14 +307,7 @@ class qt(QMainWindow):
         
         # # Setting up Storage
         from camera.streamer.h5storageserver import h5Server
-        # print("Starting Storage Server")
-        # now = datetime.now()
-        # filename = now.strftime("%Y%m%d%H%M%S") + ".hdf5"
-        # hdf5 = h5Server("C:\\temp\\" + filename)
-        # print("Starting Storage Server")
-        # hdf5.start()
-        
-        # Create camera interface
+       
         # Create camera interface
         from camera.capture.blackflycapture import blackflyCapture
         print("Starting Capture")
@@ -380,92 +315,10 @@ class qt(QMainWindow):
         print("Getting Images")
         self.camera.start()
                 
-        # # Initialize Variables
-        # frame_idx              = 0  # index to create data cube out of individual frames
-        # num_cubes_stored       = 0  # keep track of data cubes sent to storage
-        # num_cubes_generated    = 0  # keep track of data cubes generated
-        # last_time              = time.perf_counter() # keep track of time to dispay performance
-        # last_display           = time.perf_counter() # keeo track of time to display images
-        # num_frames_received    = 0  # keep track of how many captured frames reach the main program
-        # num_frames_displayed   = 0  # keep track of how many frames are displayed
-        # measured_dps           = 0  # computed in main thread, number of frames displayed per second
-        # proc_time              = 0 
-        # counter      = bin_time  = 0  
-        # min_fr = 0.0
-        # max_fr = 1.0
-
-        # # Reducing the image resolution by binning (summing up pixels)
-        # bin_x=20
-        # bin_y=20
-        # scale = (bin_x*bin_y*255)
-
-        # Binning 20 pixels of the 8bit images
-        @jit(nopython=True, fastmath=True, parallel=True)
-        def bin20(arr_in):
-            m,n,o   = np.shape(arr_in)
-            arr_tmp = np.empty((m//20,n,o), dtype='uint16')
-            arr_out = np.empty((m//20,n//20,o), dtype='uint32')
-            for i in prange(m//20):
-                arr_tmp[i,:,:] =  arr_in[i*20,:,:]    + arr_in[i*20+1,:,:]  + arr_in[i*20+2,:,:]  + arr_in[i*20+3,:,:]  + arr_in[i*20+4,:,:]  + arr_in[i*20+5,:,:]  + \
-                                arr_in[i*20+6,:,:]  + arr_in[i*20+7,:,:]  + arr_in[i*20+8,:,:]  + arr_in[i*20+9,:,:]  + arr_in[i*20+10,:,:] + arr_in[i*20+11,:,:] + \
-                                arr_in[i*20+12,:,:] + arr_in[i*20+13,:,:] + arr_in[i*20+14,:,:] + arr_in[i*20+15,:,:] + arr_in[i*20+16,:,:] + arr_in[i*20+17,:,:] + \
-                                arr_in[i*20+18,:,:] + arr_in[i*20+19,:,:]
-
-            for j in prange(n//20):
-                arr_out[:,j,:]  = arr_tmp[:,j*20,:]    + arr_tmp[:,j*20+1,:]  + arr_tmp[:,j*20+2,:]  + arr_tmp[:,j*20+3,:]  + arr_tmp[:,j*10+4,:]  + arr_tmp[:,j*20+5,:]  + \
-                                arr_tmp[:,j*20+6,:]  + arr_tmp[:,j*20+7,:]  + arr_tmp[:,j*20+8,:]  + arr_tmp[:,j*20+9,:]  + arr_tmp[:,j*20+10,:] + arr_tmp[:,j*20+11,:] + \
-                                arr_tmp[:,j*20+12,:] + arr_tmp[:,j*20+13,:] + arr_tmp[:,j*10+14,:] + arr_tmp[:,j*20+15,:] + arr_tmp[:,j*20+16,:] + arr_tmp[:,j*20+17,:] + \
-                                arr_tmp[:,j*20+18,:] + arr_tmp[:,j*20+19,:] 
-            return arr_out
-
-        @vectorize(['uint16(uint8, float32, uint8)'], nopython = True, fastmath = True)
-        def correction(background, flatfield, data_cube):
-            return np.multiply(np.subtract(data_cube,background),flatfield)
-        
-      
-
-  #this code determines the curve fit of the background
-        """ def wavelength(background):
-        width  = 720
-        height = 540
-        xmin = 0
-        xmax = width - 1
-        nx =  xmax - xmin + 1
-        ymin = 0
-        ymax = height - 1
-        ny =  ymax - ymin + 1
-        x, y = np.linspace(xmin, xmax, nx), np.linspace(ymin, ymax, ny)
-        X, Y = np.meshgrid(x, y)
-
-        # Our function to fit is going to be a sum of two-dimensional polynomials
-        def poly2(x, y, x0, y0, a0, a1, a2, a3, a4, a5):
-            x_c = x - x0
-            y_c = y - y0
-            return a0 + a1*(x_c) + a2*y_c + a3*x_c**2 + a4 * x_c * y_c + a5 * y_c**2
-        def _poly2(M, *args):
-            x, y = M
-            arr = np.zeros(x.shape)
-            arr = poly2(x, y, *args[0:8])
-            return arr
-        
-        image = background
-
-        # define 2nd order fit parameters
-        p2 = [720//2, 540//2, 720//2, 540//2, 720//2,  540//2,  720//2,  540//2]
-        # we need to ravel the meshgrids of X, Y points to a pair of 1-D arrays.
-        xdata = np.vstack((X.ravel(), Y.ravel()))
-        ydata = image.ravel()
-        # do the fit using our custom poly2 function
-        popt, pcov = curve_fit(_poly2, xdata, ydata, p2)
-        # create curve fit
-        curvefit = poly2(X, Y, *popt[0:8])
-
-        ######****************** FIT ******************######
-        # curve fit divided by maximum image value to generate matrix from 0 to 1
-        fit = curvefit/255.
-
-        return(fit) """ 
-        
+       # Binning 20 pixels of the 8bit images
+        # @jit(nopython=True, fastmath=True, parallel=True)        
+        # @vectorize(['uint16(uint8, float32, uint8)'], nopython = True, fastmath = True)
+       
         stop=self.stop
         i=0
         while(not stop):
@@ -643,10 +496,6 @@ class qt(QMainWindow):
             background_indx = np.argmin(inten)
 
             background = self.data_cube[background_indx,:,:]
-
-            #determine curve fit of background in real time - takes too long
-            #flatfield[13,:,:] = wavelength(data_cube[background_indx,:,:])
-
             index_array = np.arange(0, 14)
             array_plus_index = index_array + background_indx + 1
             ind = array_plus_index%14
@@ -658,19 +507,20 @@ class qt(QMainWindow):
     def bin20(self):
         arr_in=self.data_cube_corr
         m,n,o   = np.shape(arr_in)
-        arr_tmp = np.empty((m//20,n,o), dtype='uint16')
-        arr_out = np.empty((m//20,n//20,o), dtype='uint32')
-        for i in prange(m//20):
-            arr_tmp[i,:,:] =  arr_in[i*20,:,:]    + arr_in[i*20+1,:,:]  + arr_in[i*20+2,:,:]  + arr_in[i*20+3,:,:]  + arr_in[i*20+4,:,:]  + arr_in[i*20+5,:,:]  + \
-                                arr_in[i*20+6,:,:]  + arr_in[i*20+7,:,:]  + arr_in[i*20+8,:,:]  + arr_in[i*20+9,:,:]  + arr_in[i*20+10,:,:] + arr_in[i*20+11,:,:] + \
-                                arr_in[i*20+12,:,:] + arr_in[i*20+13,:,:] + arr_in[i*20+14,:,:] + arr_in[i*20+15,:,:] + arr_in[i*20+16,:,:] + arr_in[i*20+17,:,:] + \
-                                arr_in[i*20+18,:,:] + arr_in[i*20+19,:,:]
+        arr_tmp = np.empty((m,n//20,o), dtype='uint16')
+        arr_out = np.empty((m,n//20,o//20), dtype='uint32')
+        for i in prange(n//20):
+            arr_tmp[:,i,:] =  arr_in[:,i*20,:]    + arr_in[:,i*20+1,:]  + arr_in[:,i*20+2,:]  + arr_in[:,i*20+3,:]  + arr_in[:,i*20+4,:]  + arr_in[:,i*20+5,:]  + \
+                            arr_in[:,i*20+6,:]  + arr_in[:,i*20+7,:]  + arr_in[:,i*20+8,:]  + arr_in[:,i*20+9,:]  + arr_in[:,i*20+10,:] + arr_in[:,i*20+11,:] + \
+                            arr_in[:,i*20+12,:] + arr_in[:,i*20+13,:] + arr_in[:,i*20+14,:] + arr_in[:,i*20+15,:] + arr_in[:,i*20+16,:] + arr_in[:,i*20+17,:] + \
+                            arr_in[:,i*20+18,:] + arr_in[:,i*20+19,:]
 
-        for j in prange(n//20):
-             arr_out[:,j,:]  = arr_tmp[:,j*20,:]    + arr_tmp[:,j*20+1,:]  + arr_tmp[:,j*20+2,:]  + arr_tmp[:,j*20+3,:]  + arr_tmp[:,j*10+4,:]  + arr_tmp[:,j*20+5,:]  + \
-                                arr_tmp[:,j*20+6,:]  + arr_tmp[:,j*20+7,:]  + arr_tmp[:,j*20+8,:]  + arr_tmp[:,j*20+9,:]  + arr_tmp[:,j*20+10,:] + arr_tmp[:,j*20+11,:] + \
-                                arr_tmp[:,j*20+12,:] + arr_tmp[:,j*20+13,:] + arr_tmp[:,j*10+14,:] + arr_tmp[:,j*20+15,:] + arr_tmp[:,j*20+16,:] + arr_tmp[:,j*20+17,:] + \
-                                arr_tmp[:,j*20+18,:] + arr_tmp[:,j*20+19,:] 
+        for j in prange(o//20):
+            arr_out[:,:,j]  = arr_tmp[:,:,j*20]    + arr_tmp[:,:,j*20+1]  + arr_tmp[:,:,j*20+2]  + arr_tmp[:,:,j*20+3]  + arr_tmp[:,:,j*10+4]  + arr_tmp[:,:,j*20+5]  + \
+                            arr_tmp[:,:,j*20+6]  + arr_tmp[:,:,j*20+7]  + arr_tmp[:,:,j*20+8]  + arr_tmp[:,:,j*20+9]  + arr_tmp[:,:,j*20+10] + arr_tmp[:,:,j*20+11] + \
+                            arr_tmp[:,:,j*20+12] + arr_tmp[:,:,j*20+13] + arr_tmp[:,:,j*10+14] + arr_tmp[:,:,j*20+15] + arr_tmp[:,:,j*20+16] + arr_tmp[:,:,j*20+17] + \
+                            arr_tmp[:,:,j*20+18] + arr_tmp[:,:,j*20+19] 
+       
         self.data_cube_corr= arr_out
         return self.data_cube_corr
 
@@ -796,22 +646,7 @@ class qt(QMainWindow):
 
         return (frame_ratio)
 
-    def bin20(arr_in):
-        m,n,o   = np.shape(arr_in)
-        arr_tmp = np.empty((m,n//20,o), dtype='uint16')
-        arr_out = np.empty((m,n//20,o//20), dtype='uint32')
-        for i in prange(n//20):
-            arr_tmp[:,i,:] =  arr_in[:,i*20,:]    + arr_in[:,i*20+1,:]  + arr_in[:,i*20+2,:]  + arr_in[:,i*20+3,:]  + arr_in[:,i*20+4,:]  + arr_in[:,i*20+5,:]  + \
-                            arr_in[:,i*20+6,:]  + arr_in[:,i*20+7,:]  + arr_in[:,i*20+8,:]  + arr_in[:,i*20+9,:]  + arr_in[:,i*20+10,:] + arr_in[:,i*20+11,:] + \
-                            arr_in[:,i*20+12,:] + arr_in[:,i*20+13,:] + arr_in[:,i*20+14,:] + arr_in[:,i*20+15,:] + arr_in[:,i*20+16,:] + arr_in[:,i*20+17,:] + \
-                            arr_in[:,i*20+18,:] + arr_in[:,i*20+19,:]
-
-        for j in prange(o//20):
-            arr_out[:,:,j]  = arr_tmp[:,:,j*20]    + arr_tmp[:,:,j*20+1]  + arr_tmp[:,:,j*20+2]  + arr_tmp[:,:,j*20+3]  + arr_tmp[:,:,j*10+4]  + arr_tmp[:,:,j*20+5]  + \
-                            arr_tmp[:,:,j*20+6]  + arr_tmp[:,:,j*20+7]  + arr_tmp[:,:,j*20+8]  + arr_tmp[:,:,j*20+9]  + arr_tmp[:,:,j*20+10] + arr_tmp[:,:,j*20+11] + \
-                            arr_tmp[:,:,j*20+12] + arr_tmp[:,:,j*20+13] + arr_tmp[:,:,j*10+14] + arr_tmp[:,:,j*20+15] + arr_tmp[:,:,j*20+16] + arr_tmp[:,:,j*20+17] + \
-                            arr_tmp[:,:,j*20+18] + arr_tmp[:,:,j*20+19] 
-        return arr_out
+   
 ############################################################### First page End ###############################################
 
 ############################################################### Second page Start ############################################
